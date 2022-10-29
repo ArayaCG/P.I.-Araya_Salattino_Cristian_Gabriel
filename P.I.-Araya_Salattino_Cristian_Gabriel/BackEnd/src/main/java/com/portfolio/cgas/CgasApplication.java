ffc7c6afd97a0d099e4d8e5c5bcd75caea22c3c3
Knutt64 <arayag64@gmail.com>
2022-10-29 20:08:37 -0300

    Empezando el back

---
+++ P.I.-Araya_Salattino_Cristian_Gabriel/BackEnd/src/main/java/com/portfolio/cgas/CgasApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.cgas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgasApplication.class, args);
+	}
+
+}
