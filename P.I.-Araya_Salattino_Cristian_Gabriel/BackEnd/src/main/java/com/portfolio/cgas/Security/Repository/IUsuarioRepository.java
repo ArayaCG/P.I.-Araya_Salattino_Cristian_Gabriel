ac77caf08c184a619dc15ac367a3b680ce946505
Knutt64 <arayag64@gmail.com>
2022-10-30 17:54:58 -0300

    Viendo el back

---
+++ P.I.-Araya_Salattino_Cristian_Gabriel/BackEnd/src/main/java/com/portfolio/cgas/Security/Repository/IUsuarioRepository.java
@@ -0,0 +1,15 @@
+
+package com.portfolio.cgas.Security.Repository;
+
+import com.portfolio.cgas.Security.Entity.Usuario;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUsuarioRepository extends JpaRepository<Usuario, Integer>{
+    Optional<Usuario> findByNombreUsuario(String nombreUsuario);
+    
+    boolean existsByNombreUsuario(String nombreUsuario);
+    boolean existsByEmail(String Email);
+}
---
+++ P.I.-Araya_Salattino_Cristian_Gabriel/BackEnd/src/main/java/com/portfolio/cgas/Security/Service/RolService.java
@@ -0,0 +1,25 @@
+
+package com.portfolio.cgas.Security.Service;
+
+import com.portfolio.cgas.Security.Entity.Rol;
+import com.portfolio.cgas.Security.Enums.RolNombre;
+import com.portfolio.cgas.Security.Repository.IRolRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class RolService {
+    @Autowired
+    IRolRepository irolRepository;
+    
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return irolRepository.findByRolNombre(rolNombre);
+    }
+    
+    public void save(Rol rol){
+        irolRepository.save(rol);
+    }
+}
