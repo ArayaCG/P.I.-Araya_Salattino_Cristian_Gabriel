a771bad6773002eb933ad359492b518385a4158e
Knutt64 <arayag64@gmail.com>
2022-10-31 03:03:18 -0300

    Back

---
+++ P.I.-Araya_Salattino_Cristian_Gabriel/BackEnd/src/main/java/com/portfolio/cgas/Security/Controller/Mensaje.java
@@ -0,0 +1,27 @@
+
+package com.portfolio.cgas.Security.Controller;
+
+
+public class Mensaje {
+    private String mensaje;
+    
+    //costructores
+
+    public Mensaje() {
+    }
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    //getset
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    
+}
